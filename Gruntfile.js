3237dc775e69e5b703f10202a43ebfbbbe8dd062
Jaime Gomez <jgomez@sproutloud.com>
2017-12-04 13:23:26 -0500

    Master the essentials - grunt - adding grunt-watch and grunt-uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,34 @@
 module.exports = function(grunt) {
     // Adding the default task for running Grunt
     grunt.initConfig({
-        name: 'class'
+        name: 'class',
+        watch: {
+            files: ['*.js', '*.css'],
+            task: ['updated']
+        },
+        uglify: {
+            build: {
+                src: ['index.js', 'logger.js'],
+                dest: 'dist/bundle.js'
+            }
+        }
     });
-    grunt.registerTask('default', (name) => {
+
+    // Loading the task for yglify and watch
+    // Command to generate the bundle.js: 'grunt uglify'
+    // Commmand to init the grunt watch: 'grunt watch'
+    // npm i -s grunt-contrib-watch grunt-contrib-uglify
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // task to be launched when a js or css file gets updated
+    grunt.registerTask('updated', () => {
+        grunt.log.writeln(`It updated again`);
+    });
+
+    // task to be called from console
+    // Command to run grunt default task from console 'grunt'
+    grunt.registerTask('default', () => {
         grunt.log.writeln(`Hello, ${grunt.config.get('name')}`);
     });
 };
